f15ce0bab5f63e74bb03a8516d97bdbe7599a232
natswatch <nathaliec@me.com>
2020-08-18 22:22:21 -0700

    edit date

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+
+// brings out text input upon click of p element
 $(".list-group").on("click", "p", function(){
   var text = $(this)
   .text()
@@ -55,6 +57,9 @@ $(".list-group").on("click", "p", function(){
   textInput.trigger("focus");
 });
 
+
+
+// saves and replaces with new text after an event, blur in this case
 $(".list-group").on("blur", "textarea", function() {
   //get the textarea's current value/text
   var text = $(this)
@@ -85,7 +90,57 @@ $(".list-group").on("blur", "textarea", function() {
 
 });
 
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
 
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function(){
+  //get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+  
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
